54c13e970105a0785084b4370762e1c0cc2968f3
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-07-24 07:31:43 +0300

    impl: find max num in array

--- JavaScript/example.js
+++ JavaScript/example.js
@@ -1,1 +1,14 @@
 console.log('hello')
+
+
+function getMax(array) {
+	let max = array[0];
+	array.forEach((item) => {
+		if(item > max) max = item;
+	})
+
+	return max;
+}
+
+
+console.log(getMax([1, 2, 3, 4, 5, 50, 6, 10, 100, 1000]));
